003d1297641011c3cbeafc577284d845827330c8
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2012-02-01 16:09:03 +0200

    Add graceful gunicorn reload

--- fabdeploy/gunicorn.py
+++ fabdeploy/gunicorn.py
@@ -1,8 +1,14 @@
+from fabric.api import puts, sudo
+
+import supervisor
 from .task import Task
 from .utils import upload_config_template
 
 
-__all__ = ['push_config']
+__all__ = [
+    'push_config',
+    'reload_with_supervisor',
+]
 
 
 class PushConfig(Task):
@@ -10,3 +16,14 @@ class PushConfig(Task):
         upload_config_template('gunicorn.conf.py', context=self.conf)
 
 push_config = PushConfig()
+
+
+class ReloadWithSupervisor(Task):
+    def do(self):
+        pid = int(supervisor.pid_program.run(program='gunicorn'))
+        if not pid:
+            puts('Gunicorn is not running (pid=%s). Skipping...' % pid)
+            return
+        sudo('kill -HUP %s' % pid)
+
+reload_with_supervisor = ReloadWithSupervisor()
