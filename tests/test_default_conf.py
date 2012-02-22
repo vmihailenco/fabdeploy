d84cdab563917b8487875b4c8de9ef5af0b3dd74
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2012-02-22 14:15:40 +0200

    Fix tests

--- tests/test_default_conf.py
+++ tests/test_default_conf.py
@@ -10,19 +10,17 @@ def test_update_conf():
     assert conf.host == 'localhost'
     assert conf.instance_name == 'fabdeploy'
 
-    version = conf.version
-    version_path = '/home/fabdeploy/%s' % version
-
     assert conf.home_path == '/home/fabdeploy'
-    assert conf.version_path == version_path
-    assert conf.src_path == version_path + '/src'
-    assert conf.project_path == version_path + '/src'
-    assert conf.django_path == version_path + '/src'
-    assert conf.env_path == version_path + '/env'
-    assert conf.etc_path == version_path + '/env/etc'
-    assert conf.var_path == version_path + '/env/var'
-    assert conf.log_path == version_path + '/env/var/log'
-    assert conf.backup_path == version_path + '/env/var/backup'
+    assert conf.version_path == '/home/fabdeploy/%s' % conf.version
+    assert conf.src_path == conf.version_path + '/src'
+    assert conf.project_path == conf.version_path + '/src'
+    assert conf.django_path == conf.version_path + '/src'
+    assert conf.shared_path == conf.home_path + '/shared'
+    assert conf.env_path == conf.version_path + '/env'
+    assert conf.etc_path == conf.shared_path + '/etc'
+    assert conf.var_path == conf.shared_path + '/var'
+    assert conf.log_path == conf.var_path + '/log'
+    assert conf.backup_path == conf.var_path + '/backup'
 
     assert conf.time_format == '%Y.%m.%d-%H.%M.%S'
     assert conf.sudo_user == 'root'
