5fec6a926f1d9cd63bbb0dd32ed8e337d46811a3
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2012-02-22 14:01:36 +0200

    Add gunicorn.push_nginx_config

--- fabdeploy/gunicorn.py
+++ fabdeploy/gunicorn.py
@@ -1,12 +1,15 @@
 from fabric.api import puts, sudo
 
 import supervisor
+from .containers import conf
 from .task import Task
 from .utils import upload_config_template
+from .nginx import PushConfigTask as PushNginxConfigTask
 
 
 __all__ = [
     'push_config',
+    'push_nginx_config',
     'reload_with_supervisor',
 ]
 
@@ -16,6 +19,14 @@ class PushConfig(Task):
         upload_config_template('gunicorn.conf.py', context=self.conf)
 
 push_config = PushConfig()
+
+
+class PushNginxConfig(PushNginxConfigTask):
+    @conf
+    def config_template(self):
+        return 'nginx_gunicorn.config'
+
+push_nginx_config = PushNginxConfig()
 
 
 class ReloadWithSupervisor(Task):
